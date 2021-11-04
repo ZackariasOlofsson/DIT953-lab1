d32848f28a35bfbcba90ee8ddfb91500b9f09e1c
ZackariasOlofsson <zackariasolofsson@gmail.com>
2021-11-04 08:59:15 +0100

    påbörjan av interface

--- CarInterface.java
+++ CarInterface.java
@@ -1,2 +1,15 @@
+import java.awt.*;
+
 public interface CarInterface {
+    // metoder
+    public int getNrDoors();
+    public double getEnginePower();
+    public double getCurrentSpeed();
+    public Color getColor();
+    public void setColor();
+    public void startEngine();
+    public void stopEngine();
+
+    // konstanter
+
 }
