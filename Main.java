efaf521d57741a037ff78095eaf9bfc84e158292
mathilda1999 <mathilda.gustafsson@hotmail.se>
2021-11-05 13:04:00 +0100

    har skrivit in instansvariabler, konstruktorer och metoder i superklassen Car, och skapat två nya subklasser

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     // en mainmetod
     public static void main(String[] args) {
         Nyvolvo mycar = new Nyvolvo();
-        System.out.println(mycar.getNrDoors()); // instansvariabler private men metoder public(protected?) för att kunn
+        System.out.println(mycar.getNrDoors()); // instansvariabler private men metoder public(protected?)
         System.out.println(mycar.getEnginePower());
     }
 
